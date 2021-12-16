b29940fe2a0eb594ac4e20f89ab5ab3d21c4f1cc
lucklyYong <luckyzangvip@163.com>
2021-12-16 09:50:31 +0800

    vant引入改为本地引入

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -32,7 +32,7 @@ module.exports = {
     'vue-router': 'VueRouter',
     'vuex': 'Vuex',
     'axios': 'axios',
-    'vant': 'vant',
+    // 'vant': 'vant',
     'echarts': 'echarts'
   },
   output: {
