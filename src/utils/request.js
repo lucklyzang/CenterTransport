e2f9d040763a09142d942e33099a6fec609a49a1
lucklyzang <luckyzangvip@163.com>
2022-08-02 20:38:24 +0800

    请求地址变更

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,139 +9,139 @@ Vue.use(Dialog);
 import { setStore } from '@/common/js/utils'
 // http://blink.blinktech.cn/trans 测试地址
 // http://blinktech.cn/trans 正式地址
+// http://show.blinktech.cn/trans
 // http://new.blinktech.cn/trans 临时生产地址
 const service = axios.create({
-  baseURL: 'http://blink.blinktech.cn/trans', //接口基础地址
-  retry: 2, // 网络请求异常后，重试次数
-  retryDelay: 1000, // 每次重试间隔时间
-  shouldRetry: (err) => true // 重试条件
+    baseURL: 'http://show.blinktech.cn/trans', //接口基础地址
+    retry: 2, // 网络请求异常后，重试次数
+    retryDelay: 1000, // 每次重试间隔时间
+    shouldRetry: (err) => true // 重试条件
 });
 axios.defaults.withCredentials = true;
 // request interceptor
 service.interceptors.request.use(
-  config => {
-    if (store.getters.token) {
-      config.headers['Authorization'] = store.getters.token
-    };
-    // 请求头添加模板信息
-    if (store.getters.templateType) {
-      config.headers['REQUEST_TEMPLATE'] = store.getters.templateType
-    };
-    // 请求头添加设备唯一标识号(IMEI)
-    if (!IsPC()) {
-      config.headers['MOBILE_MARK'] = ''
-      // try {
-      //   if (window.android.getImei()) {
-      //     config.headers['MOBILE_MARK'] = window.android.getImei()
-      //   } else {
-      //     config.headers['MOBILE_MARK'] = ''
-      //   }
-      // } catch (err) {
-      // }
-    };
-    return config
-  },
+    config => {
+        if (store.getters.token) {
+            config.headers['Authorization'] = store.getters.token
+        };
+        // 请求头添加模板信息
+        if (store.getters.templateType) {
+            config.headers['REQUEST_TEMPLATE'] = store.getters.templateType
+        };
+        // 请求头添加设备唯一标识号(IMEI)
+        if (!IsPC()) {
+            config.headers['MOBILE_MARK'] = ''
+                // try {
+                //   if (window.android.getImei()) {
+                //     config.headers['MOBILE_MARK'] = window.android.getImei()
+                //   } else {
+                //     config.headers['MOBILE_MARK'] = ''
+                //   }
+                // } catch (err) {
+                // }
+        };
+        return config
+    },
 
-  error => {
-    console.log(error)
-    return Promise.reject(error)
-  }
+    error => {
+        console.log(error)
+        return Promise.reject(error)
+    }
 );
 
 // response interceptor
 service.interceptors.response.use(
-  response => {
-    // 获取响应头token,并存储到vuex和localStorage中
-    if (response.headers['token']) {
-      store.commit('changeToken', response.headers['token']);
-      setStore('questToken', response.headers['token']);
-    };
-    if (response.headers.hasOwnProperty('offline')) {
-      Dialog.alert({
-        message: '账号已在其它设备登录,你已被强制下线!',
-        closeOnPopstate: false
-      }).then(() => {
-      });
-      if(store.getters.globalTimer) {window.clearInterval(store.getters.globalTimer)};
-      removeAllLocalStorage();
-      // 退出信标服务器连接
-      try {
-        window.android.logOut()
-      } catch (err) {
-        Toast(`${err}`)
-      };
-      setTimeout(() => {
-        router.push({path: '/'})
-      },2000);
-      return response
-    };
-    if (!response.headers.hasOwnProperty('token')) {
-      if (!store.getters.overDueWay) {
-        Toast('token已过期,请重新登录');
-        if(store.getters.globalTimer) {window.clearInterval(store.getters.globalTimer)};
-        if(store.getters.globalCircleTimer) {window.clearInterval(store.getters.globalCircleTimer)};
-        removeAllLocalStorage();
-        // 退出信标服务器连接
-        try {
-          window.android.logOut()
-        } catch (err) {
-          Toast(`${err}`)
+    response => {
+        // 获取响应头token,并存储到vuex和localStorage中
+        if (response.headers['token']) {
+            store.commit('changeToken', response.headers['token']);
+            setStore('questToken', response.headers['token']);
         };
-        setTimeout(() => {
-          router.push({path: '/'})
-        },2000);
-      }
-    };
-    return response
-  },
-  (err) => {
-    var config = err.config;
-    // 判断是否配置了重试
-    if(!config || !config.retry) {
-      if (err.message == 'Network Error') {
-        return Promise.reject({message:'网络错误'});
-      } else {
-        return Promise.reject(err);
-      }
-    };
-    if(!config.shouldRetry || typeof config.shouldRetry != 'function') {
-      if (err.message == 'Network Error') {
-        return Promise.reject({message:'网络错误'});
-      } else {
-        return Promise.reject(err);
-      }
-    };
-    //判断是否满足重试条件
-    if(!config.shouldRetry(err)) {
-      if (err.message == 'Network Error') {
-        return Promise.reject({message:'网络错误'});
-      } else {
-        return Promise.reject(err);
-      }
-    };
-    // 设置重置次数，默认为0
-    config.__retryCount = config.__retryCount || 0;
-    // 判断是否超过了重试次数
-     if(config.__retryCount > config.retry) {
-       if (err.message == 'Network Error') {
-         return Promise.reject({message:'网络错误'});
-       } else {
-         return Promise.reject(err);
-       }
-     };
-    //重试次数自增
-    config.__retryCount += 1;
-    //延时处理
-    var backoff = new Promise(function(resolve) {
-      setTimeout(function() {
-        resolve();
-      }, config.retryDelay || 1);
-    });
-    //重新发起axios请求
-    return backoff.then(function() {
-      return service(config);
-    })
-  }
+        if (response.headers.hasOwnProperty('offline')) {
+            Dialog.alert({
+                message: '账号已在其它设备登录,你已被强制下线!',
+                closeOnPopstate: false
+            }).then(() => {});
+            if (store.getters.globalTimer) { window.clearInterval(store.getters.globalTimer) };
+            removeAllLocalStorage();
+            // 退出信标服务器连接
+            try {
+                window.android.logOut()
+            } catch (err) {
+                Toast(`${err}`)
+            };
+            setTimeout(() => {
+                router.push({ path: '/' })
+            }, 2000);
+            return response
+        };
+        if (!response.headers.hasOwnProperty('token')) {
+            if (!store.getters.overDueWay) {
+                Toast('token已过期,请重新登录');
+                if (store.getters.globalTimer) { window.clearInterval(store.getters.globalTimer) };
+                if (store.getters.globalCircleTimer) { window.clearInterval(store.getters.globalCircleTimer) };
+                removeAllLocalStorage();
+                // 退出信标服务器连接
+                try {
+                    window.android.logOut()
+                } catch (err) {
+                    Toast(`${err}`)
+                };
+                setTimeout(() => {
+                    router.push({ path: '/' })
+                }, 2000);
+            }
+        };
+        return response
+    },
+    (err) => {
+        var config = err.config;
+        // 判断是否配置了重试
+        if (!config || !config.retry) {
+            if (err.message == 'Network Error') {
+                return Promise.reject({ message: '网络错误' });
+            } else {
+                return Promise.reject(err);
+            }
+        };
+        if (!config.shouldRetry || typeof config.shouldRetry != 'function') {
+            if (err.message == 'Network Error') {
+                return Promise.reject({ message: '网络错误' });
+            } else {
+                return Promise.reject(err);
+            }
+        };
+        //判断是否满足重试条件
+        if (!config.shouldRetry(err)) {
+            if (err.message == 'Network Error') {
+                return Promise.reject({ message: '网络错误' });
+            } else {
+                return Promise.reject(err);
+            }
+        };
+        // 设置重置次数，默认为0
+        config.__retryCount = config.__retryCount || 0;
+        // 判断是否超过了重试次数
+        if (config.__retryCount > config.retry) {
+            if (err.message == 'Network Error') {
+                return Promise.reject({ message: '网络错误' });
+            } else {
+                return Promise.reject(err);
+            }
+        };
+        //重试次数自增
+        config.__retryCount += 1;
+        //延时处理
+        var backoff = new Promise(function(resolve) {
+            setTimeout(function() {
+                resolve();
+            }, config.retryDelay || 1);
+        });
+        //重新发起axios请求
+        return backoff.then(function() {
+            return service(config);
+        })
+    }
 );
 
 export default service
