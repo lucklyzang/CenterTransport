cc5096601d42c2d6daa9abbb4fc816334edecc18
lucklyYong <luckyzangvip@163.com>
2023-11-30 11:11:11 +0800

    测试问题修复

--- src/pages/taskSchedulingPage/CreateAppointTask.vue
+++ src/pages/taskSchedulingPage/CreateAppointTask.vue
@@ -811,7 +811,7 @@ export default {
         proName: this.proName,   //项目名称
         assignerId: this.workerId, //分配人id
         assignerName: this.userName, // 分配人姓名
-        createType: 0,   //创建类型 0-接入，1-调度员
+        createType: 1,   //创建类型 0-接入，1-调度员
       };
       // 处理选中的检查类型
       let temporaryDateTime = this.getNowFormatDate(this.currentTaskStartTime).split(" ");
